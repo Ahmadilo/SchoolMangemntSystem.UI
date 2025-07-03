5e1d146f1f3e19582dd3756b7d8c68c313d8c981
Ahmad Aden <ahmadx222x2gmail.com>
2025-07-03 23:20:22 +0300

    Add gitignore and Delete the Rendomly tests file from repo.

--- test.js
+++
@@ -1,8 +0,0 @@
-import { TableComponent } from '/Components/Table.js';
-
-const container = document.getElementById('students-table');
-/** @type {TableComponent} */
-const table = new TableComponent(container, 'http://localhost:8000/api/students');
-
-
-table.refresh();
